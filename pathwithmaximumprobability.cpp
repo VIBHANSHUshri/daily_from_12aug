7c5704ff3869c4c9f75a98dd92e00b7835038f12
VIBHANSHUshri <vibhanshu123khare@gmail.com>
2024-08-31 23:56:39 +0530

    new

--- pathwithmaximumprobability.cpp
+++ pathwithmaximumprobability.cpp
@@ -1,26 +1,28 @@
 class Solution {
 public:
-    double maxProbability(int n, vector<vector<int>>& edges, vector<double>& succProb, int start, int end) {
-        vector<pair<int,double>> adj[n];
-        for(int i=0;i<edges.size();i++){
-            adj[edges[i][0]].push_back({edges[i][1],succProb[i]});
-            adj[edges[i][1]].push_back({edges[i][0],succProb[i]});
-        }
-        vector<double> prob(n,0.0);
-        prob[start]=1.0;
-        queue<pair<int,double>> q;
-        q.push({start,1.0});
-        while(!q.empty()){
-            auto it = q.front();
-            q.pop();
-            for(auto v:adj[it.first]){
-                if(prob[v.first]<(it.second*v.second)){
-                    prob[v.first]=it.second*v.second;
-                    q.push({v.first,prob[v.first]});
+    double maxProbability(int n, vector<vector<int>>& edges, vector<double>& succProb, int start_node, int end_node) {
+        vector<double> maxProb(n, 0.0);
+        maxProb[start_node] = 1.0;
+
+        for (int i = 0; i < n - 1; ++i) {
+            bool updated = false;
+            for (int j = 0; j < edges.size(); ++j) {
+                int u = edges[j][0];
+                int v = edges[j][1];
+                double prob = succProb[j];
+
+                if (maxProb[u] * prob > maxProb[v]) {
+                    maxProb[v] = maxProb[u] * prob;
+                    updated = true;
+                }
+                if (maxProb[v] * prob > maxProb[u]) {
+                    maxProb[u] = maxProb[v] * prob;
+                    updated = true;
                 }
             }
+            if (!updated) break;
         }
-        return prob[end];
-        //ok
+
+        return maxProb[end_node];
     }
 };
